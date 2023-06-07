8476fc53a9e97a2ea668dbbc2b1cc9b9d881f40b
knn654 <kaua.nascimento@fatec.sp.gov.br>
2023-06-07 19:23:43 -0300

    refactor: Trabalho and Orientação encapsulation

--- src/model/Orientacao.java
+++ src/model/Orientacao.java
@@ -12,4 +12,38 @@ public class Orientacao {
 		return (dia + ";" + mes + ";" + ano + ";" + pontos);
 	}
 
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getPontos() {
+		return pontos;
+	}
+
+	public void setPontos(String pontos) {
+		this.pontos = pontos;
+	}
+	
+	
+
 }
